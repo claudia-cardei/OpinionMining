fece661ea0aff06e48b6ed532a08fdb1e88be7a4
Claudia Cardei <claudia.cardei@gmail.com>
2013-05-07 15:18:28 +0300

    added fdg parsing

---
+++ src/com/java/opinionmining/fdgparsing/FDGParser.java
@@ -0,0 +1,57 @@
+package com.java.opinionmining.fdgparsing;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import uaic.webfdgro.FdgParserRoWSStub;
+import uaic.webfdgro.FdgParserRoWSStub.ParseText;
+import uaic.webfdgro.FdgParserRoWSStub.ParseTextE;
+import uaic.webfdgro.FdgParserRoWSStub.ParseTextResponseE;
+
+public class FDGParser {
+	
+	public static List<FDGNode> getFDGParserTree(String text) {
+		FdgParserRoWSStub service;
+		
+		try {
+			service = new FdgParserRoWSStub();
+			
+			ParseText parseText = new ParseText();
+			parseText.setTxt(text);
+			
+			ParseTextE process = new ParseTextE();
+			process.setParseText(parseText);
+			
+			ParseTextResponseE response = service.parseText(process);
+			String responseText = response.getParseTextResponse().get_return();
+			
+			DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+	        Document doc = docBuilder.parse(new InputSource(new StringReader(responseText)));
+	        
+	        FDGTreeBuilder builder = new FDGTreeBuilder(doc);
+	        return builder.build();
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
