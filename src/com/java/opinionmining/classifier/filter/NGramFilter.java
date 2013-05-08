7cd01abc35beb122f347b5005bfb53622ede2a03
Filip Manisor <filip.manisor@gmail.com>
2013-05-08 22:24:38 +0300

    Added nGram filter

--- src/com/java/opinionmining/classifier/filter/NGramFilter.java
+++ src/com/java/opinionmining/classifier/filter/NGramFilter.java
@@ -7,76 +7,75 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
 import org.apache.commons.lang3.mutable.MutableInt;
-
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.filters.SimpleBatchFilter;
-
-import com.java.opinionmining.database.DictionaryModel;
 import com.java.opinionmining.database.POSModel;
 import com.java.opinionmining.postagging.TaggedWord;
 import com.java.opinionmining.postagging.Tagger;
 
 
 /**
- * Filter for opinion mining. Extracts the words from the text attributes along with their 
- * frequencies (but does not only retain the more frequent ones yet), normalizes the words (lower 
- * case, character replacement) and does a dictionary check in order to obtain the relevant 
- * words. Then uses these words as attributes for the new ARFF format and encodes all the instances
- * according to them.
+ * Filter for opinion mining. Extracts nGrams from the text attributes, which it uses as attributes 
+ * for the new ARFF format and encodes all the instances accordingly.
  * 
  * @author Filip Manisor
  *
  */
-public class OpinionFilter extends SimpleBatchFilter {
+public class NGramFilter extends SimpleBatchFilter {
 	
 	private static final long serialVersionUID = 7696981018501541605L;
 	
-	private ArrayList<TaggedWord> relevantWords;
-	private Instances outputFormat;
-	private DictionaryModel dictionaryModel;
-	private POSModel posModel;
-	
+	POSModel posModel;
+	private Instances outputFormat;	
 	private String outputFile;
+	int n;
+	ArrayList<String> relevantNGrams;
 	
-	public OpinionFilter(String outputFile) {
-		relevantWords = new ArrayList<TaggedWord>();
-		dictionaryModel = new DictionaryModel();
-		posModel = new POSModel();
-		
+	
+	public NGramFilter(int n, String outputFile) {
+		posModel = new POSModel();		
+		this.n = n;
 		this.outputFile = outputFile;
+		relevantNGrams = new ArrayList<String>();
 	}
 
+	
 	public String globalInfo() {
-		return "Filter for the files used in opinion mining.";
+		return "NGram filter for the files used in opinion mining.";
 	}
-		
+	
+	
 	protected Instances determineOutputFormat(Instances inputFormat) throws Exception {
 		outputFormat = new Instances(inputFormat, 0);
+		int i = 0;
 		
 		// Set the list of relevant words
-		setRelevantWords();
+		setNGrams();
 		
 		// Delete the text attribute
 		outputFormat.deleteAttributeAt(0);
 		
-		// Add a new attribute for each word
-		for (TaggedWord taggedWord : relevantWords) {
+		// Add a new attribute for each nGram
+		System.out.println(relevantNGrams.size());
+		for (String nGram : relevantNGrams) {
 			
 			// Add the attribute to the end of the list
-			outputFormat.insertAttributeAt(new Attribute(taggedWord.getLemmaPOSTagged()),
+			outputFormat.insertAttributeAt(new Attribute(nGram.replace('|', '_')),
 					outputFormat.numAttributes() - 1);
 		}
+		
 		
 	    return outputFormat;
 	}
 
+	
 	protected Instances process(Instances inputInstances) throws Exception {
-		int numAttributes = relevantWords.size() + 1;
+		int numAttributes = relevantNGrams.size() + 1, j, k;
+		String nGram;
 
 		Instances result = new Instances(outputFormat, 0);
 		for (int i = 0; i < inputInstances.numInstances(); i++) {
@@ -96,18 +95,20 @@ public class OpinionFilter extends SimpleBatchFilter {
 			
 			// Get the tagged word list
 			List<TaggedWord> taggedWords = Tagger.getRACAITaggedWordsFromOutput(processResult);
-			
+
 			double[] values = new double[numAttributes];
-			for (TaggedWord taggedWord : taggedWords) {
-				// Normalize lemma 
-				String lemma = taggedWord.getLemma();
+			for (j = 0; j < taggedWords.size() - n + 1; j++) {
 				
-				// Dictionary check
-				if (dictionaryModel.existsWordInDictionary(lemma)) {		
-					int index = relevantWords.indexOf(taggedWord);
-					if ( index > -1 ) {
-						values[index] = 1;
-					}
+				// Form the current nGram
+				nGram = "";
+				for (k = j; k < j + n; k++) 
+					nGram += taggedWords.get(k).getLemma().toLowerCase() + "|";
+				nGram = nGram.substring(0, nGram.length() - 1);
+				
+				// Check if the nGram is in the attribute list
+				int index = relevantNGrams.indexOf(nGram);
+				if ( index > -1 ) {
+					values[index] = 1;
 				}
 			}
 			
@@ -115,6 +116,7 @@ public class OpinionFilter extends SimpleBatchFilter {
 			values[numAttributes - 1] = instance.classValue();
 			
 			result.add(new Instance(1, values));
+			
 		}
 		
 		ArffSaver saver = new ArffSaver();
@@ -125,14 +127,20 @@ public class OpinionFilter extends SimpleBatchFilter {
 		return result;
 	}
 	
-	private void setRelevantWords() {
-		Instances input = getInputFormat();
-		Map<TaggedWord, MutableInt> relevantWordMap = new HashMap<TaggedWord, MutableInt>();
+	
+	/**
+	 * Select the nGrams that will be used as attributes
+	 */
+	private void setNGrams() {
+		int i, j, k;
+		Instances input = getInputFormat();		
+		String nGram;
+		Map<String, MutableInt> relevantNGramMap = new HashMap<String, MutableInt>();
 		
-		for (int i = 0; i < input.numInstances(); i++) {
+		for (i = 0; i < input.numInstances(); i++) {
 			// Get the text attribute from each instance
-			String text = input.instance(i).stringValue(0);
-			
+			String text = input.instance(i).stringValue(0).toLowerCase();	
+						
 			// Check the database for the phrase
 			String processResult = posModel.getProcessResult(text);
 			if ( processResult == null ) {
@@ -146,27 +154,33 @@ public class OpinionFilter extends SimpleBatchFilter {
 			// Get the tagged word list
 			List<TaggedWord> taggedWords = Tagger.getRACAITaggedWordsFromOutput(processResult);
 			
-			for (TaggedWord taggedWord : taggedWords) {
-				// Normalize lemma 
-				String lemma = taggedWord.getLemma();
+			for (j = 0; j < taggedWords.size() - n + 1; j++) {
 				
-				// Dictionary check
-				if (dictionaryModel.existsWordInDictionary(lemma)) { 
-					MutableInt n = relevantWordMap.get(taggedWord);
-					if ( n == null ) {
-						relevantWordMap.put(taggedWord, new MutableInt(1));
-					}
-					else {
-						n.increment();
-					}
+				// Form the current nGram
+				nGram = "";
+				for (k = j; k < j + n; k++) 
+					nGram += taggedWords.get(k).getLemma().toLowerCase() + "|";
+				nGram = nGram.substring(0, nGram.length() - 1);
+				
+				// Add the nGram to the map
+				MutableInt n = relevantNGramMap.get(nGram);
+				if ( n == null ) {
+					relevantNGramMap.put(nGram, new MutableInt(1));
 				}
-			}			
+				else {
+					n.increment();
+				}
+			}
+			
 		}
 		
-		Set<Entry<TaggedWord, MutableInt>> entrySet = relevantWordMap.entrySet();
+		Set<Entry<String, MutableInt>> entrySet = relevantNGramMap.entrySet();
 		 
-		for (Entry<TaggedWord, MutableInt> word : entrySet) {
-			relevantWords.add(word.getKey());
+		// Select the most frequent nGrams
+		MutableInt threshold = new MutableInt(4);
+		for (Entry<String, MutableInt> word : entrySet) {
+			if ( word.getValue().compareTo(threshold) > 0 )
+				relevantNGrams.add(word.getKey());
 		}
 	}
 }
